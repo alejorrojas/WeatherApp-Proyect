21e376f71466f3cb2d85fee250396d6e4103d7b6
Alejo <alejoivanrojas@gmail.com>
2022-02-12 16:36:18 -0300

    store bug fixed 2

--- src/Redux/store.ts
+++ src/Redux/store.ts
@@ -3,6 +3,5 @@ import thunk from "redux-thunk";
 import { ciudades } from "./Interfaces";
 import rootReducer from "./reducer";
 
-const store: Store<ciudades[], AnyAction> = createStore(rootReducer, applyMiddleware(thunk));
+export const store: Store<ciudades[], AnyAction> = createStore(rootReducer, applyMiddleware(thunk));
 
-export default store
